1f8d809a20420332412a1f30cc3b958aff64bbe8
KwonGa <rltmd1994@gmail.com>
2025-07-28 20:18:26 +0900

    add test_file

--- 1_1260_DFS_BFS/DFS_BFS_test.py
+++ 1_1260_DFS_BFS/DFS_BFS_test.py
@@ -1,20 +1,20 @@
 from collections import deque
 
 n, m, v = map(int, input().split())
-print(f"n: {n}, m: {m}, v: {v}")
+print(f"n:{n}, m:{m}, v:{v}")
 
 graph = [[] for _ in range(n+1)]
 for _ in range(m):
     start, end = map(int, input().split())
-    graph[start].append(end);
-    graph[end].append(start);
+    graph[start].append(end)
+    graph[end].append(start)
 
 for i in range(n+1):
-    graph[i].sort();
+    graph[i].sort()
 print(graph)
 
 def dfs(v, visited):
-    visited[v] = True;
+    visited[v] = True
     print(v, end=' ')
     for next_node in graph[v]:
         if not visited[next_node]:
@@ -22,16 +22,16 @@ def dfs(v, visited):
             dfs(next_node, visited)
 
 def bfs(v, visited):
-    queue = deque();
-    queue.append(v);
-    visited[v] = True;
+    visited[v] = True
+    queue = deque()
+    queue.append(v)
     while queue:
-        now = queue.popleft();
-        print(now, end=' ');
+        now = queue.popleft()
+        print(now, end = ' ')
         for next_node in graph[now]:
             if not visited[next_node]:
-                visited[next_node] = True;
-                queue.append(next_node);
+                visited[next_node] = True
+                queue.append(next_node)
 
 visited_dfs = [False] * (n+1)
 dfs(v, visited_dfs)
