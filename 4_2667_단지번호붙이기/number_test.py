c8add0944771e16342616fbfd7b99e340f9d37c5
GiSeungKwon <rltmd1994@gmail.com>
2025-08-07 23:54:02 +0900

    0807

--- 4_2667_단지번호붙이기/number_test.py
+++ 4_2667_단지번호붙이기/number_test.py
@@ -1,29 +1,31 @@
-from collections import deque
-
 n = int(input())
 graph = [list(map(int, input().strip())) for _ in range(n)]
 print(graph)
-
-number = [[] for _ in range(n)]
-print(number)
-index = 0
+visited = [[False] * (n) for _ in range(n)]
+print(visited)
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+count = 0
+
 def dfs(x, y):
-    queue = deque()
-    queue.append([x, y])
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx<0 or ny<0 or nx<=n or ny<=n:
-                continue
-            if graph[nx][ny] == 0:
-                continue
-            if graph[nx][ny] == 1:
-                global index
-                number[index].append(1)
-print(number)
+    global count
+    visited[x][y] = True
+    count += 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0<=nx<n and 0<=ny<n and graph[nx][ny] == 1 and not visited[nx][ny]:
+            dfs(nx, ny)
+
+result = []
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1 and not visited[i][j]:
+            dfs(i, j)
+            result.append(count)
+            count = 0
+print(len(result))
+for i in range(len(result)):
+    print(result[i])
