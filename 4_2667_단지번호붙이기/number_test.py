6642ecf94dc6234f0f276c3025f8a654c6ffceb4
KwonGa <rltmd1994@gmail.com>
2025-08-13 12:03:58 +0900

    0813

--- 4_2667_단지번호붙이기/number_test.py
+++ 4_2667_단지번호붙이기/number_test.py
@@ -1,11 +1,11 @@
 n = int(input())
-count = 0
-result = []
 graph = [list(map(int, input().strip())) for _ in range(n)]
 visited = [[False] * n for _ in range(n)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
+
+count = 0
 
 def dfs(x, y):
     global count
@@ -14,10 +14,11 @@ def dfs(x, y):
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if 0<=nx<n and 0<=ny<n and not visited[nx][ny] and graph[nx][ny] == 1:
+        if 0<=nx<n and 0<=ny<n and graph[nx][ny] == 1 and not visited[nx][ny]:
             visited[nx][ny] = True
             dfs(nx, ny)
 
+result = []
 for i in range(n):
     for j in range(n):
         if not visited[i][j] and graph[i][j] == 1:
