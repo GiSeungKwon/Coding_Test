5c7b93d1e75402bdafa7515003fa9029d2900621
KwonGa <rltmd1994@gmail.com>
2025-09-04 19:57:17 +0900

    0904

--- 2_2178_미로탐색/maze_test.py
+++ 2_2178_미로탐색/maze_test.py
@@ -2,23 +2,23 @@ from collections import deque
 
 n, m = map(int, input().split())
 maze = [list(map(int, input().strip())) for _ in range(n)]
-print(f"n:{n}, m:{m}\nmaze: {maze}")
+print(maze)
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def dfs(x, y):
     queue = deque()
-    queue.append([x, y])
+    queue.append((x, y))
+
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0<=nx<n and 0<=ny<m and maze[nx][ny] == 1:
-                queue.append([nx, ny])
+            if 0<=nx<n and 0<=ny<m and maze[nx][ny]==1:
+                queue.append((nx, ny))
                 maze[nx][ny] = maze[x][y] + 1
-                dfs(nx, ny)
     return maze[n-1][m-1]
 
 print(dfs(0, 0))
