f4896391ef07f1b7a3dbd1ca056d4e4f8d52a3f7
KwonGa <rltmd1994@gmail.com>
2025-09-23 20:10:33 +0900

    0923

--- 9_2468_안전 영역/safeZone.py
+++ 9_2468_안전 영역/safeZone.py
@@ -1,46 +1,34 @@
 from collections import deque
 
-# 입력
-m, n, h = map(int, input().split())
-box = [[[0]*m for _ in range(n)] for _ in range(h)]
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-for k in range(h):
+max_height = max(map(max, graph))  # 지역의 최대 높이
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y, h, visited):
+    queue = deque()
+    queue.append((x, y))
+    visited[x][y] = True
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n:
+                if not visited[nx][ny] and graph[nx][ny] > h:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
+
+answer = 0
+for h in range(max_height + 1):  # 강수량 0 ~ 최대 높이
+    visited = [[False] * n for _ in range(n)]
+    count = 0
     for i in range(n):
-        box[k][i] = list(map(int, input().split()))
+        for j in range(n):
+            if not visited[i][j] and graph[i][j] > h:
+                bfs(i, j, h, visited)
+                count += 1
+    answer = max(answer, count)
 
-# 좌우 앞뒤 상하
-dx = [-1, 1, 0, 0, 0, 0]
-dy = [0, 0, -1, 1, 0, 0]
-dz = [0, 0, 0, 0, -1, 1]
-
-# 익은 토마토 위치 저장
-queue = deque()
-for z in range(h):
-    for y in range(n):
-        for x in range(m):
-            if box[z][y][x] == 1:
-                queue.append((z, y, x))
-
-# BFS
-while queue:
-    z, y, x = queue.popleft()
-    for d in range(6):
-        nz = z + dz[d]
-        ny = y + dy[d]
-        nx = x + dx[d]
-        if 0 <= nz < h and 0 <= ny < n and 0 <= nx < m:
-            if box[nz][ny][nx] == 0:
-                box[nz][ny][nx] = box[z][y][x] + 1
-                queue.append((nz, ny, nx))
-
-# 결과 확인 
-days = 0
-for z in range(h):
-    for y in range(n):
-        for x in range(m):
-            if box[z][y][x] == 0:
-                print(-1)
-                exit()
-            days = max(days, box[z][y][x])
-
-print(days - 1)  # 첫날은 1로 시작했으니 -1
+print(answer)
