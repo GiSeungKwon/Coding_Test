01bb40c26a6989aca1a26159ba58204aa8f06be3
GiSeungKwon <rltmd1994@gmail.com>
2026-02-24 16:42:52 +0900

    0224_1615

--- 1단계/1_1260_DFS_BFS/DFS_BFS_test.py
+++ 1단계/1_1260_DFS_BFS/DFS_BFS_test.py
@@ -1,13 +1,35 @@
 from collections import deque
 
 n, m, v = map(int, input().split())
-print(n, m, v)
-
 graph = [[] for _ in range(n+1)]
 
 for i in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-print(graph)
 
+def bfs(v, visited):
+    queue = deque()
+    queue.append(v)
+    visited[v] = True
+    while queue:
+        now = queue.popleft()
+        print(now, end=" ")
+        for next_node in graph[now]:
+            if not visited[next_node]:
+                visited[next_node] = True
+                queue.append(next_node)
+
+def dfs(v, visited):
+    print(v, end=" ")
+    visited[v] = True
+    for next_node in graph[v]:
+        if not visited[next_node]:
+            visited[next_node] = True
+            dfs(next_node, visited)
+
+visited_bfs = [False] * (n+1)
+visited_dfs = [False] * (n+1)
+bfs(v, visited_bfs)
+print()
+dfs(v, visited_dfs)
