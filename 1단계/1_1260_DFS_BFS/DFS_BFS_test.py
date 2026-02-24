95cedfa30ff171f9832d1d9554d156fe1fc43edd
GiSeungKwon <rltmd1994@gmail.com>
2026-02-24 16:21:29 +0900

    0224_1615

--- 1단계/1_1260_DFS_BFS/DFS_BFS_test.py
+++ 1단계/1_1260_DFS_BFS/DFS_BFS_test.py
@@ -1,5 +1,13 @@
 from collections import deque
 
-n, m, v = input().split()
+n, m, v = map(int, input().split())
 print(n, m, v)
+
+graph = [[] for _ in range(n+1)]
+
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+print(graph)
 
