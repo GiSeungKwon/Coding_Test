c6c946ebaf002e9f07bda50196d70a9db4a8748c
GiSeungKwon <rltmd1994@gmail.com>
2026-02-24 22:06:02 +0900

    0224_2205

--- 1단계/3_2606_바이러스/virus_test.py
+++ 1단계/3_2606_바이러스/virus_test.py
@@ -3,5 +3,28 @@ from collections import deque
 n = int(input())
 m = int(input())
 
+graph = [[] for _ in range(n+1)]
+print(f"graph: {graph}")
+visited = [False] * (n+1)
+print(f"visited: {visited}")
+
 for _ in range(m):
     a, b = map(int, input().split())
+    graph[a].append(b)
+print(f"graph: {graph}")
+
+queue = deque()
+queue.append(1)
+visited[1] = True
+while queue:
+    now = queue.popleft()
+    for next_node in graph[now]:
+        if not visited[next_node]:
+            visited[next_node] = True
+            queue.append(next_node)
+print(visited)
+count = 0
+for vi in visited:
+    if vi == True:
+        count += 1
+print(count - 1)
