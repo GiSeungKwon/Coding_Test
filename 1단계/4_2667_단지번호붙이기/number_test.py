782d5a2da02a465806afc1e5b3898487f399e66f
GiSeungKwon <rltmd1994@gmail.com>
2026-02-24 22:59:53 +0900

    0224_2259

--- 1단계/4_2667_단지번호붙이기/number_test.py
+++ 1단계/4_2667_단지번호붙이기/number_test.py
@@ -1,28 +1,43 @@
+from collections import deque
+
 n = int(input())
-graph = [list(map(int, input().strip())) for _ in range(n)]
-visited = [[False]*n for _ in range(n)]
+graph = [list(map(int, input())) for _ in range(n)]
+for i in range(n):
+    print(graph[i])
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+visited = [[False] * n for _ in range(n)]
+# for i in range(n):
+#     print(visited[i])
 
-def dfs(x, y):
-    global count
-    count += 1
-    visited[x][y] = True
-    for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
-        if 0<=nx<n and 0<=ny<n and graph[nx][ny] == 1 and not visited[nx][ny]:
-            visited[nx][ny] = True
-            dfs(nx, ny)
+dy, dx = [1, -1, 0, 0], [0, 0, -1, 1]
 
 result = []
+danji_count = 0
 for i in range(n):
     for j in range(n):
         if not visited[i][j] and graph[i][j] == 1:
+            danji_count += 1
+            queue = deque()
             count = 0
-            dfs(i, j)
+            queue.append((i, j))
+            print()
+            print(f"queue.append((i:{i}, j:{j}))")
+            while queue:
+                y, x = queue.popleft()
+                print(f"queue.popleft() y:{y}, x:{x}, count:{count}")
+                for k in range(4):
+                    ny, nx = y+dy[k], x+dx[k]
+                    if 0<=ny<n and 0<=nx<n and graph[ny][nx]==1 :
+                        if not visited[ny][nx]:
+                            count += 1
+                            visited[ny][nx] = True
+                            queue.append((ny, nx))
+                            print(f"queue: {queue}")
+                            for i in range(n):
+                                print(f"{visited[i]}")
             result.append(count)
-
-print(len(result))
-for item in result:
-    print(item)
+print(f"danji_count: {danji_count}")
+print(f"result: {sorted(result)}")
+# 출력
+# 단지수
+# 각 단지 수를 오름차순
