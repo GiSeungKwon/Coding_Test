0c98b1064328f96d98938c5337c5e95e7844611a
GiSeungKwon <rltmd1994@gmail.com>
2026-02-24 22:19:19 +0900

    0224_2219

--- 1단계/3_2606_바이러스/virus_test.py
+++ 1단계/3_2606_바이러스/virus_test.py
@@ -4,9 +4,7 @@ n = int(input())
 m = int(input())
 
 graph = [[] for _ in range(n+1)]
-print(f"graph: {graph}")
 visited = [False] * (n+1)
-print(f"visited: {visited}")
 
 for _ in range(m):
     a, b = map(int, input().split())
@@ -23,8 +21,9 @@ while queue:
             visited[next_node] = True
             queue.append(next_node)
 print(visited)
-count = 0
-for vi in visited:
-    if vi == True:
-        count += 1
-print(count - 1)
+print(visited.count(True)-1)
+# count = 0
+# for vi in visited:
+#     if vi == True:
+#         count += 1
+# print(count - 1)
