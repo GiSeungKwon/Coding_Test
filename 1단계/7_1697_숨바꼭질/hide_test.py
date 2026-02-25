014083456c76fd277ee9a77eae1049588bf8eb99
GiSeungKwon <rltmd1994@gmail.com>
2026-02-25 19:50:30 +0900

    0225_1950

--- 1단계/7_1697_숨바꼭질/hide_test.py
+++ 1단계/7_1697_숨바꼭질/hide_test.py
@@ -1,5 +1,4 @@
 from collections import deque
-
 n, k = map(int, input().split())
 max = 1000001
 graph = [[-1] for _ in range(max)]
@@ -7,15 +6,12 @@ visited = [False] * max
 
 queue = deque()
 queue.append((n, 0))
-visited[n] = True
 
 while queue:
-    print(f"queue: {queue}")
     now, count = queue.popleft()
     if now == k:
         print(count)
         break
     for next_node in (now+1, now-1, now*2):
         if 0<=next_node<max and not visited[next_node]:
-            visited[next_node] = True
             queue.append((next_node, count + 1))
