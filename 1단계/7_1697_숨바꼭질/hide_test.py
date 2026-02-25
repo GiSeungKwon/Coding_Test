1c00d2df81fd70c08b466ed638f77d55339bd0b7
GiSeungKwon <rltmd1994@gmail.com>
2026-02-25 19:33:27 +0900

    0225_1733

--- 1단계/7_1697_숨바꼭질/hide_test.py
+++ 1단계/7_1697_숨바꼭질/hide_test.py
@@ -1,23 +1,21 @@
 from collections import deque
 
 n, k = map(int, input().split())
-graph = [-1] * (2*k)
-visited = [False] * (2*k)
+max = 1000001
+graph = [[-1] for _ in range(max)]
+visited = [False] * max
 
-def bfs():
-    queue = deque()
-    queue.append(n)
-    visited[n] = True
-    graph[n] = 0
+queue = deque()
+queue.append((n, 0))
+visited[n] = True
 
-    while queue:
-        now = queue.popleft()
-        if now == k:
-            return graph[now]
-        for next_node in (now-1, now+1, now*2):
-            if 0<=next_node<2*k and not visited[next_node]:
-                visited[next_node] = True
-                graph[next_node] = graph[now] + 1
-                queue.append(next_node)
-    return -1
-print(bfs())
+while queue:
+    print(f"queue: {queue}")
+    now, count = queue.popleft()
+    if now == k:
+        print(count)
+        break
+    for next_node in (now+1, now-1, now*2):
+        if 0<=next_node<max and not visited[next_node]:
+            visited[next_node] = True
+            queue.append((next_node, count + 1))
