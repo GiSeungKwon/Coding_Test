9085cf9f47e44f09f0c29a1dd29fecc93359c760
GiSeungKwon <rltmd1994@gmail.com>
2026-02-25 20:08:57 +0900

    0225_2008

--- 1단계/8_5014_스타트링크/startLink_test.py
+++ 1단계/8_5014_스타트링크/startLink_test.py
@@ -1,2 +1,12 @@
 from collections import deque
 
+f, s, g, u, d = map(int, input().split())
+
+queue = deque()
+queue.append((s, 0))
+visited = [False] * (f+1)
+
+while queue:
+    now, count = queue.popleft()
+    for next_node in (now + u, now - d):
+        if 0 <= next_node <= 1000001 and :
