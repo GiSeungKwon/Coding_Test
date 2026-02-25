0a74c41a8efbe9f75859ac91c1b076caa6a1fe99
GiSeungKwon <rltmd1994@gmail.com>
2026-02-25 20:19:16 +0900

    0225_2019

--- 1단계/8_5014_스타트링크/startLink_test.py
+++ 1단계/8_5014_스타트링크/startLink_test.py
@@ -1,12 +1,21 @@
 from collections import deque
 
 f, s, g, u, d = map(int, input().split())
-
-queue = deque()
-queue.append((s, 0))
 visited = [False] * (f+1)
 
-while queue:
-    now, count = queue.popleft()
-    for next_node in (now + u, now - d):
-        if 0 <= next_node <= 1000001 and :
+def solution(f, s, g, u, d, visited):
+    queue = deque()
+    queue.append((s, 0))
+    visited[s] = True
+
+    while queue:
+        now, count = queue.popleft()
+        if now == g:
+            return count
+        for next_node in (now + u, now - d):
+            if 0 <= next_node <= f and not visited[next_node]:
+                visited[next_node] = True
+                queue.append((next_node, count + 1))
+    return "use the stairs"
+
+print(solution(f, s, g, u, d, visited))
