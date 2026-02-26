93babc84a7df896d42883f6936ee914f91ccaf17
GiSeungKwon <rltmd1994@gmail.com>
2026-02-26 19:04:07 +0900

    0226 1904

--- 1단계/9_2468_안전 영역/safeZone_test.py
+++ 1단계/9_2468_안전 영역/safeZone_test.py
@@ -2,6 +2,8 @@ from collections import deque
 
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
+dy, dx = [1,-1,0,0], [0,0,-1,1]
+
 max_values = []
 max_vale = 0
 
@@ -10,23 +12,32 @@ for i in range(n):
     max_values.append(max(graph[i]))
 max_value = max(max_values)
 
-rain_graph = [[0] * n for _ in range(n)]
-# 물에 잠기면 0, 물에 안잠기면 1(안전 영역)
-
 list_count = []
 # m: 비가 온 정도 -> m 이하이면 잠기는거
-for m in range(1, max_value + 1):
-    print(f"m: {m}")
+for rain in range(1, max_value + 1):
+    print(f"rain: {rain}")
+    # 물에 잠기면 0, 물에 안잠기면 1(안전 영역)
+    rain_graph = [[0] * n for _ in range(n)]
+    visited = [[False] * n for _ in range(n)]
     count = 0
     for i in range(n):
         for j in range(n):
-            if m < graph[i][j]:
+            if rain < graph[i][j]:
                 rain_graph[i][j] = 1
+            else:
+                visited[i][j] = True
     print(f"rain_graph")
     for i in range(n):
         print(rain_graph[i])
-
-    visited = [[False] * n for _ in range(n)]
-
+    print(f"visited")
     for i in range(n):
         print(visited[i])
+
+    queue = deque()
+    for i in range(n):
+        for j in range(n):
+            if rain_graph[i][j] == 1 and not visited[i][j]:
+                visited[i][j] = True
+                queue.append((i, j))
+                while queue:
+                    y, x = queue.popleft()
