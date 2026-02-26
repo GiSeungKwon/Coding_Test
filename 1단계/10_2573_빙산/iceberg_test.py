c6034f854958ed161436e607191e7565ea825db0
GiSeungKwon <rltmd1994@gmail.com>
2026-02-26 20:05:58 +0900

    0226 1805

--- 1단계/10_2573_빙산/iceberg_test.py
+++ 1단계/10_2573_빙산/iceberg_test.py
@@ -1,5 +1,17 @@
 n, m = map(int, input().split())
+dy, dx = [1, -1, 0, 0], [0, 0, -1, 1]
 graph = [list(map(int, input().split())) for _ in range(n)]
 for i in range(n):
     print(graph[i])
 
+list_ice = []
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] != 0:
+            count = 0
+            for m in range(4):
+                ny, nx = i+dy[m], j+dx[m]
+                if 0<=ny<n and 0<=nx<m and graph[ny][nx] == 0:
+                    count += 1
+            list_ice.append((i,j,count))
+print(list_ice)
