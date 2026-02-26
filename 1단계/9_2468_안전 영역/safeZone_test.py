6eea6e45d0ac06e540c81bc4ec4187b50b08eb73
GiSeungKwon <rltmd1994@gmail.com>
2026-02-26 19:26:03 +0900

    0226 1926

--- 1단계/9_2468_안전 영역/safeZone_test.py
+++ 1단계/9_2468_안전 영역/safeZone_test.py
@@ -8,13 +8,13 @@ max_values = []
 max_vale = 0
 
 # 최대 값 추출 Logic
-for i in range(n):
-    max_values.append(max(graph[i]))
+for m in range(n):
+    max_values.append(max(graph[m]))
 max_value = max(max_values)
 
 list_count = []
 # m: 비가 온 정도 -> m 이하이면 잠기는거
-for rain in range(1, max_value + 1):
+for rain in range(max_value + 1):
     print(f"rain: {rain}")
     # 물에 잠기면 0, 물에 안잠기면 1(안전 영역)
     rain_graph = [[0] * n for _ in range(n)]
@@ -26,12 +26,6 @@ for rain in range(1, max_value + 1):
                 rain_graph[i][j] = 1
             else:
                 visited[i][j] = True
-    print(f"rain_graph")
-    for i in range(n):
-        print(rain_graph[i])
-    print(f"visited")
-    for i in range(n):
-        print(visited[i])
 
     queue = deque()
     for i in range(n):
@@ -41,3 +35,13 @@ for rain in range(1, max_value + 1):
                 queue.append((i, j))
                 while queue:
                     y, x = queue.popleft()
+                    for k in range(4):
+                        ny, nx = y+dy[k], x+dx[k]
+                        if 0<=ny<n and 0<=nx<n and rain_graph[ny][nx] == 1:
+                            if not visited[ny][nx]:
+                                visited[ny][nx] = True
+                                queue.append((ny, nx))
+                count += 1
+    list_count.append(count)
+print(f"list_count: {list_count}")
+print(max(list_count))
