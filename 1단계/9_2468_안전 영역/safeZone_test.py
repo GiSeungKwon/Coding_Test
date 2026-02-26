a258f43ee2044e1d9c68ccb5cad0c8699c23dd5d
GiSeungKwon <rltmd1994@gmail.com>
2026-02-26 18:49:20 +0900

    0226 1849

--- 1단계/9_2468_안전 영역/safeZone_test.py
+++ 1단계/9_2468_안전 영역/safeZone_test.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
 max_values = []
@@ -8,18 +10,23 @@ for i in range(n):
     max_values.append(max(graph[i]))
 max_value = max(max_values)
 
-rain_graph = [[0]*n for _ in range(n)]
+rain_graph = [[0] * n for _ in range(n)]
 # 물에 잠기면 0, 물에 안잠기면 1(안전 영역)
 
 list_count = []
 # m: 비가 온 정도 -> m 이하이면 잠기는거
-for m in range(1, max_value+1):
+for m in range(1, max_value + 1):
     print(f"m: {m}")
     count = 0
     for i in range(n):
         for j in range(n):
-            if graph[i][j] <= m:
-                rain_graph[i][j] = 0
+            if m < graph[i][j]:
+                rain_graph[i][j] = 1
     print(f"rain_graph")
     for i in range(n):
         print(rain_graph[i])
+
+    visited = [[False] * n for _ in range(n)]
+
+    for i in range(n):
+        print(visited[i])
