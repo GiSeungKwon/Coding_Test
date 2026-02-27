5d1c7f2a44f75b380056fdcb6c79c3fe475eef83
GiSeungKwon <rltmd1994@gmail.com>
2026-02-27 15:49:37 +0900

    0227 1549

--- 1단계/11_9205_맥주 마시면서 걸어가기/beerWalk_test.py
+++ 1단계/11_9205_맥주 마시면서 걸어가기/beerWalk_test.py
@@ -1,7 +1,14 @@
 from collections import deque
 test_case = int(input())
 for _ in range(test_case):
+    # convenience_n: 2
     convenience_n = int(input())
-    print(f"convenience_n: {convenience_n}")
+    # start_coord: [0, 0]
     start_coord = list(map(int, input().split()))
-    print(f"start_coord: {start_coord}")
+    # convenience_coord: [[1000, 0], [1000, 1000]]
+    convenience_coord = [list(map(int, input().split())) for _ in range(convenience_n)]
+    # end_coord: [2000, 1000]
+    end_coord = list(map(int, input().split()))
+
+
+    print()
