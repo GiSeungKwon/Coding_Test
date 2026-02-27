0fc307696305b8c736a80ae3e171a5e2b9b6ea2b
GiSeungKwon <rltmd1994@gmail.com>
2026-02-27 16:46:10 +0900

    0227 1646

--- 1단계/12_14503_로봇 청소기/robotCleaner_test.py
+++ 1단계/12_14503_로봇 청소기/robotCleaner_test.py
@@ -2,4 +2,6 @@ n, m = map(int, input().split())
 print(f"n:{n}, m:{m}")
 y, x, d = map(int, input().split())
 print(f"y:{y}, x:{x}, d:{d}")
-
+graph = [list(map(int, input().split())) for _ in range(n)]
+for i in range(n):
+    print(graph[i])
