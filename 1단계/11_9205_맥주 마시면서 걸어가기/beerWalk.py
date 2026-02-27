43e417923df21c0ad54b467ca38e9efa34ebab5b
GiSeungKwon <rltmd1994@gmail.com>
2026-02-27 15:42:41 +0900

    0227 1542

--- 1단계/11_9205_맥주 마시면서 걸어가기/beerWalk.py
+++ 1단계/11_9205_맥주 마시면서 걸어가기/beerWalk.py
@@ -1,26 +1,39 @@
 from collections import deque
 
-def manhattan(p1, p2):
-    return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
+test_case = int(input())
+print(f"test_case: {test_case}")
+print()
 
-t = int(input())
+def manhattan(a, b):
+    return abs(a[0]-b[0])+abs(a[1]-b[1])
 
-for _ in range(t):
-    n = int(input())
-    graph = [tuple(map(int, input().split())) for _ in range(n+2)]
-    visited = [False] * (n+2)
+for _ in range(test_case):
+    convenience_n = int(input())
+    print(f"convenience_n: {convenience_n}")
+    start_coordinate = list(map(int, input().split()))
+    print(f"start_coordinate: {start_coordinate}")
+    convenience_coordinate_batch = [list(map(int, input().split())) for _ in range(convenience_n)]
+    print(f"convenience_coordinate_batch: {convenience_coordinate_batch}")
+    end_coordinate = list(map(int, input().split()))
+    print(f"end_coordinate: {end_coordinate}")
 
-    def bfs():
-        visited[0] = True
-        queue = deque()
-        queue.append(0)
-        while queue:
-            now = queue.popleft()
-            if now == n+1:
-                return True
-            for next_node in range(n+2):
-                if not visited[next_node] and manhattan(graph[now], graph[next_node]) <= 1000:
-                    visited[next_node] = True
-                    queue.append(next_node)
-        return False
-    print("happy" if bfs() else "sad")
+    visited = [False] * convenience_n
+    queue = deque()
+    queue.append(start_coordinate)
+    flag = False
+
+    while queue:
+        y, x = queue.popleft()
+        if manhattan([y, x], end_coordinate) <= 1000:
+            flag = True
+            break
+        for convenience_index in range(convenience_n):
+            if manhattan([y, x], convenience_coordinate_batch[convenience_index]) <= 1000 and not visited[convenience_index]:
+                visited[convenience_index] = True
+                queue.append(convenience_coordinate_batch[convenience_index])
+
+    if flag:
+        print("happy")
+    else:
+        print("sad")
+    print()
