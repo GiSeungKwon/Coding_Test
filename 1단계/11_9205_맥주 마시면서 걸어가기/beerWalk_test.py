2410418bc20cb100aa117044a9a96ad5a248491c
GiSeungKwon <rltmd1994@gmail.com>
2026-02-27 15:32:14 +0900

    0227 1532

--- 1단계/11_9205_맥주 마시면서 걸어가기/beerWalk_test.py
+++ 1단계/11_9205_맥주 마시면서 걸어가기/beerWalk_test.py
@@ -2,21 +2,38 @@ from collections import deque
 
 test_case = int(input())
 print(f"test_case: {test_case}")
-convenience_n = int(input())
-print(f"convenience_n: {convenience_n}")
-start_coordinate = list(map(int, input().split()))
-print(f"start_coordinate: {start_coordinate}")
-convenience_coordinate = [list(map(int, input().split())) for _ in range(convenience_n)]
-print(f"convenience_coordinate: {convenience_coordinate}")
-end_coordinate = list(map(int, input().split()))
-print(f"end_coordinate: {end_coordinate}")
+print()
 
 def manhattan(a, b):
     return abs(a[0]-b[0])+abs(a[1]-b[1])
 
-visited = [False] * convenience_n
-queue = deque()
-queue.append(start_coordinate)
+for _ in range(test_case):
+    convenience_n = int(input())
+    print(f"convenience_n: {convenience_n}")
+    start_coordinate = list(map(int, input().split()))
+    print(f"start_coordinate: {start_coordinate}")
+    convenience_coordinate_batch = [list(map(int, input().split())) for _ in range(convenience_n)]
+    print(f"convenience_coordinate_batch: {convenience_coordinate_batch}")
+    end_coordinate = list(map(int, input().split()))
+    print(f"end_coordinate: {end_coordinate}")
 
-while queue:
-    y, x = queue.popleft()
+    visited = [False] * convenience_n
+    queue = deque()
+    queue.append(start_coordinate)
+    flag = False
+
+    while queue:
+        y, x = queue.popleft()
+        if manhattan([y, x], end_coordinate) <= 1000:
+            flag = True
+            break
+        for convenience_index in range(convenience_n):
+            if manhattan([y, x], convenience_coordinate_batch[convenience_index]) <= 1000 and not visited[convenience_index]:
+                visited[convenience_index] = True
+                queue.append(convenience_coordinate_batch[convenience_index])
+
+    if flag:
+        print("happy")
+    else:
+        print("sad")
+    print()
