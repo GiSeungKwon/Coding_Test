46947b017f9815015fee36f6d16f2b2f8ba49b54
GiSeungKwon <rltmd1994@gmail.com>
2026-02-27 14:21:21 +0900

    0227 1421

--- 1단계/11_9205_맥주 마시면서 걸어가기/beerWalk_test.py
+++ 1단계/11_9205_맥주 마시면서 걸어가기/beerWalk_test.py
@@ -2,8 +2,9 @@ test_case = int(input())
 print(f"test_case: {test_case}")
 convenience_n = int(input())
 print(f"convenience_n: {convenience_n}")
-start_coordinate = map(int, input().split())
-
+start_coordinate = list(map(int, input().split()))
+print(f"start_coordinate: {start_coordinate}")
 convenience_coordinate = [list(map(int, input().split())) for _ in range(convenience_n)]
-end_coordinate = [list(map(int, input().split()))]
-print()
+print(f"convenience_coordinate: {convenience_coordinate}")
+end_coordinate = list(map(int, input().split()))
+print(f"end_coordinate: {end_coordinate}")
