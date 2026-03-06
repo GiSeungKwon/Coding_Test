379fcbc76f3eeae4f533d4397541ac455ace5cf7
GiSeungKwon <rltmd1994@gmail.com>
2026-03-06 15:59:36 +0900

    0306 1533

--- 1단계/12_14503_로봇 청소기/robotCleaner_test.py
+++ 1단계/12_14503_로봇 청소기/robotCleaner_test.py
@@ -1,68 +1,38 @@
 n, m = map(int, input().split())
-print(f"n:{n}, m:{m}")
 r, c, d = map(int, input().split())
-print(f"r:{r}, c:{c}, d:{d}")
-map = [list(map(int, input().split())) for _ in range(n)]
+map = [list(map(int,input().split())) for _ in range(n)]
 count = 0
-print()
-print(f"count: {count}")
-print(f"r:{r}, c:{c}")
+dr, dc = [-1, 0, 1, 0], [0, 1, 0, -1]
+
 for i in range(n):
     print(map[i])
-print()
 
-# 북 동 남 서 / dr[d], dc[d] 현재 방향을 나타냄
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
-exit_flag = False
-count = 0
 while True:
-
-    count += 1
-    if count == 10:
-        break
-
-    if exit_flag:
-        break
-    # 현재 칸 청소 됨?
     if map[r][c] == 0:
-        # 현재 칸 청소
-        map[r][c] = "■"
+        map[r][c] = 2
         count += 1
-        print(f"count: {count}")
-        print(f"r:{r}, c:{c}")
-        for map_row in range(n):
-            print(map[map_row])
-        print()
 
-    # 주변 4칸 탐색
+    clean_flag = False
+    # 현재 칸 주변 4방향 탐색
     for i in range(4):
-        print(f"i:{i}, d:{d}")
-        print(f"r:{r} + dr[{i}]:{dr[i]} = {r+dr[i]}")
-        print(f"c:{c} + dc[{i}]:{dc[i]} = {c+dc[i]}")
-        check_r, check_c = r+dr[i], c+dc[i]
-        print(f"check_r:{check_r}, check_c:{check_c}")
-        # 청소 안된 칸 있음
-        if map[check_r][check_c] == 0:
-            print(f"map[{check_r}][{check_c}]: {map[check_r][check_c]} == 0")
-            # 반시계 방향 90도
-            d = (d + 3) % 4
-            # 진행 방향 칸이 청소 안된 칸이면 전진
-            nr, nc = r+dr[d], c+dc[d]
-            if map[nr][nc] == 0:
-                r, c = nr, nc
-                break
-        # 청소 안된 칸 없음
+        nr, nc = r+dr[i], c+dc[i]
+        # 청소할 칸이 있다
+        if map[nr][nc] == 0:
+            clean_flag = True
+
+    # 청소할 칸이 있으면
+    if clean_flag:
+        d = (d +3) % 4
+        nr, nc = r + dr[d], c + dc[d]
+        if 0 <= nr < n and 0 <= nc < m and map[nr][nc] == 0:
+            r, c = nr, nc
+    # 청소할 칸이 없으면
+    else:
+        nd = (d + 2) % 4
+        nr, nc = r + dr[nd], c + dc[nd]
+        if 0 <= nr < n and 0 <= nc < m and map[nr][nc] != 1:
+            r, c = nr, nc
         else:
-            print(f"map[{check_r}][{check_c}]: {map[check_r][check_c]} != 0")
-            # 후진 방향
-            d = (d + 2) % 4
-            nr, nc = r+dr[d], c+dc[d]
-            # 진행 방향 칸이 벽이면
-            if map[nr][nc] == 1:
-                exit_flag = True
-                break
-            # 후진 가능하면
-            else:
-                r, c = nr, nc
-                break
+            print("정지")
+            break
+print(count)
