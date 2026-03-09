bff5fe84fcf9a6d6a8f2db8075aa176764fe71d9
GiSeungKwon <rltmd1994@gmail.com>
2026-03-09 13:43:12 +0900

    0309 1343

--- 2단계/1_13460_구슬탈출2/escpaeDice.py
+++ 2단계/1_13460_구슬탈출2/escpaeDice.py
@@ -1,67 +1,59 @@
 from collections import deque
 
+n, m = map(int, input().split())
+map = [list(input().strip()) for _ in range(n)]
+# 상하좌우
+dr, dc = [1, -1, 0, 0], [0, 0, 1, -1]
+# R, O, B 좌표 구하기
+red_coord, O_coord, blue_coord = [], [], []
+for i in range(n):
+    for j in range(m):
+        if map[i][j] == 'R': red_coord = [i, j]
+        elif map[i][j] == 'O': O_coord = [i, j]
+        elif map[i][j] == 'B': blue_coord = [i, j]
 
-def move(y, x, dy, dx, board):
-    cnt = 0
-    # 벽(#)을 만나거나 구멍(O)에 들어가기 전까지 계속 이동
-    while board[y + dy][x + dx] != '#' and board[y][x] != 'O':
-        y += dy
-        x += dx
-        cnt += 1
-    return y, x, cnt
+visited = [[[[False] * m for _ in range(n)] for _ in range(m)] for _ in range(n)]
+# 초기 위치 방문 처리
+visited[red_coord[0]][red_coord[1]][blue_coord[0]][blue_coord[1]] = True
 
+dist = 0
+queue = deque()
+queue.append([red_coord[0], red_coord[1], blue_coord[0], blue_coord[1], dist])
 
-def bfs(board, N, M, red, blue):
-    # 방문 여부: visited[red_y][red_x][blue_y][blue_x]
-    visited = [[[[False] * M for _ in range(N)] for _ in range(M)] for _ in range(N)]
-    q = deque()
-    q.append((*red, *blue, 0))  # (ry, rx, by, bx, depth)
-    visited[red[0]][red[1]][blue[0]][blue[1]] = True
+def move(r, c, dr, dc):
+    count = 0
+    while map[r + dr][c + dc] != '#' and map[r][c] != 'O':
+        r += dr
+        c += dc
+        count += 1
+    return r, c, count
 
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 상, 하, 좌, 우
+# BFS 내부
+while queue:
+    ry, rx, by, bx, dist = queue.popleft()
+    if dist >= 10: break
 
-    while q:
-        ry, rx, by, bx, depth = q.popleft()
-        if depth >= 10:
-            break
-        for dy, dx in directions:
-            nry, nrx, rc = move(ry, rx, dy, dx, board)
-            nby, nbx, bc = move(by, bx, dy, dx, board)
+    for i in range(4):
+        nry, nrx, r_cnt = move(ry, rx, dr[i], dc[i])
+        nby, nbx, b_cnt = move(by, bx, dr[i], dc[i])
 
-            # 파란 구슬이 구멍에 들어가면 실패
-            if board[nby][nbx] == 'O':
-                continue
-            # 빨간 구슬이 구멍에 들어가면 성공
-            if board[nry][nrx] == 'O':
-                return depth + 1
+        # 파랑 빠지면 무시
+        if map[nby][nbx] == 'O':
+            continue
 
-            # 두 구슬이 같은 칸에 도착하면 → 이동 거리 큰 쪽을 한 칸 뒤로
-            if nry == nby and nrx == nbx:
-                if rc > bc:
-                    nry -= dy
-                    nrx -= dx
-                else:
-                    nby -= dy
-                    nbx -= dx
+        # 빨강만 빠지면 성공!
+        if map[nry][nrx] == 'O':
+            print(dist + 1)
+            exit()
 
-            # 방문하지 않은 상태라면 큐에 추가
-            if not visited[nry][nrx][nby][nbx]:
-                visited[nry][nrx][nby][nbx] = True
-                q.append((nry, nrx, nby, nbx, depth + 1))
-    return -1
+        if nry == nby and nrx == nbx:  # 겹쳤을 때 처리
+            if r_cnt > b_cnt:
+                nry -= dr[i]
+                nrx -= dc[i]
+            else:
+                nby -= dr[i]
+                nbx -= dc[i]
 
-if __name__ == "__main__":
-    N, M = map(int, input().split())
-    board = [list(map(int, input().strip())) for _ in range(N)]
-    red = blue = None
-
-    for i in range(N):
-        for j in range(M):
-            if graoh[i][j] == 'R':
-                red = (i, j)
-                graoh[i][j] = '.'  # 빈칸 처리
-            elif graoh[i][j] == 'B':
-                blue = (i, j)
-                board[i][j] = '.'
-
-    print(bfs(board, N, M, red, blue))
+        if not visited[nry][nrx][nby][nbx]:
+            visited[nry][nrx][nby][nbx] = True
+            queue.append((nry, nrx, nby, nbx, dist + 1))
