fe41992c2414c895a4a0e2a30e3d81c41f42b6d6
GiSeungKwon <rltmd1994@gmail.com>
2026-03-09 18:25:39 +0900

    0309 1825

--- 2단계/1_13460_구슬탈출2/escpaeDice_test.py
+++ 2단계/1_13460_구슬탈출2/escpaeDice_test.py
@@ -5,3 +5,8 @@ map = [list(input().strip()) for _ in range(n)]
 
 dy, dx = [-1, 0, 1, 0], [0, 1, 0, -1]
 visited = [[[[False] * m for _ in range(n)] for _ in range(m)] for _ in range(n)]
+
+red, blue = [], []
+for i in range(n):
+    for j in range(m):
+        if
