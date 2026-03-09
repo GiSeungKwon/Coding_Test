560183e0d187990a6e10fe5bb083cd8393d453bd
GiSeungKwon <rltmd1994@gmail.com>
2026-03-09 18:24:38 +0900

    0309 17824

---
+++ 2단계/1_13460_구슬탈출2/escpaeDice_test.py
@@ -0,0 +1,7 @@
+from collections import deque
+
+n, m = map(int, input().split())
+map = [list(input().strip()) for _ in range(n)]
+
+dy, dx = [-1, 0, 1, 0], [0, 1, 0, -1]
+visited = [[[[False] * m for _ in range(n)] for _ in range(m)] for _ in range(n)]
