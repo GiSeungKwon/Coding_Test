a0a2835782c30a0680366c9df540e9ab5de89cc6
GiSeungKwon <rltmd1994@gmail.com>
2026-03-10 11:14:40 +0900

    0310 1114

--- 2단계/1_13460_구슬탈출2/escpaeDice.py
+++ 2단계/1_13460_구슬탈출2/escpaeDice.py
@@ -1,67 +1,61 @@
 from collections import deque
-from copy import deepcopy
+test_case = int(input())
+for _ in range(test_case):
+    n, m = map(int, input().split())
+    board = [list(input().strip()) for _ in range(n)]
+    visited = [[[[False] * m for _ in range(n)] for _ in range(m)] for _ in range(n)]
 
-n, m = map(int, input().split())
-map = [list(input().strip()) for _ in range(n)]
+    red, blue = [], []
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == 'R': red = [i, j]
+            if board[i][j] == 'B': blue = [i, j]
 
-dr, dc = [-1, 0, 1, 0], [0, 1, 0, -1]
-visited = [[[[False] * m for _ in range(n)] for _ in range(m)] for _ in range(n)]
-
-red, blue = [], []
-for i in range(n):
-    for j in range(m):
-        if map[i][j] == 'R': red = [i, j]
-        elif map[i][j] == 'B': blue = [i, j]
-visited[red[0]][red[1]][blue[0]][blue[1]] = True
-
-def move(r, c, dr, dc):
     count = 0
-    while map[r+dr][c+dc] != '#':
-        r += dr
-        c += dc
-        count += 1
-        if map[r][c] == 'O':
+    queue = deque()
+    queue.append([red[0], red[1], blue[0], blue[1], count])
+    visited [red[0]] [red[1]] [blue[0]] [blue[1]] = True
+
+    dr, dc = [-1, 0, 1, 0], [0, 1, 0, -1]
+
+    def move(r, c, dr, dc):
+        count = 0
+        while board[r+dr][c+dc] != '#':
+            r += dr
+            c += dc
+            count += 1
+            if board[r][c] == 'O':
+                break
+        return r, c, count
+    flag = False
+    while queue:
+        flag = False
+        index = 1
+        now_red_r, now_red_c, now_blue_r, now_blue_c, count = queue.popleft()
+        if 10 <= count:
             break
-    return r, c, count
 
-count = 0
-queue = deque()
-queue.append([red[0], red[1], blue[0], blue[1], count])
-
-while queue:
-    print(queue)
-    dir = ['↑', '→', '↓', '←']
-    simulation_map = deepcopy(map)
-    for sim_i in range(n):
-        print(simulation_map[sim_i])
-    now_red_r, now_red_c, now_blue_r, now_blue_c, count = queue.popleft()
-
-    if 10 <= count:
-        print(-1)
-        exit()
-
-    if map[now_blue_r][now_blue_c] == 'O':
-        continue
-    if map[now_red_r][now_red_c] == 'O':
-        print(f"도착 {count}")
-        exit()
-
-    for i in range(4):
-        next_red_r, next_red_c, red_count = move(now_red_r, now_red_c, dr[i], dc[i])
-        next_blue_r, next_blue_c, blue_count = move(now_blue_r, now_blue_c, dr[i], dc[i])
-        print(f"{dir[i]} 방향으로 이동")
-        for sim_i in range(n):
-            print(simulation_map[sim_i])
-
-        if next_red_r == next_blue_r and next_red_c == next_blue_c:
-            if red_count < blue_count:
-                next_blue_r -= dr[i]
-                next_blue_c -= dc[i]
-            else:
-                next_red_r -= dr[i]
-                next_red_c -= dc[i]
-        if not visited[next_red_r][next_red_c][next_blue_r][next_blue_c]:
-            visited[next_red_r][next_red_c][next_blue_r][next_blue_c] = True
-            queue.append([next_red_r, next_red_c, next_blue_r, next_blue_c, count + 1])
-            print(f"queue.append([{next_red_r}, {next_red_c}, {next_blue_r}, {next_blue_c}, {count + 1}]) -> {queue}")
-print(-1)
+        for i in range(4):
+            next_red_r, next_red_c, red_count = move(now_red_r, now_red_c, dr[i], dc[i])
+            next_blue_r, next_blue_c, blue_count = move(now_blue_r, now_blue_c, dr[i], dc[i])
+            if board[next_blue_r][next_blue_c] == 'O':
+                continue
+            if board[next_red_r][next_red_c] == 'O':
+                print(f"내 정답: {count + 1}", end = " -> ")
+                flag = True
+                break
+            if next_red_r == next_blue_r and next_red_c == next_blue_c:
+                if red_count < blue_count:
+                    next_blue_r -= dr[i]
+                    next_blue_c -= dc[i]
+                else:
+                    next_red_r -= dr[i]
+                    next_red_c -= dc[i]
+            if not visited[next_red_r][next_red_c][next_blue_r][next_blue_c]:
+                visited[next_red_r][next_red_c][next_blue_r][next_blue_c] = True
+                queue.append([next_red_r, next_red_c, next_blue_r, next_blue_c, count + 1])
+        if flag:
+            break
+    if not flag:
+        print(f"내 정답: -1", end = " -> ")
+    print(f"정답: {input()}")
